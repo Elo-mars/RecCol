d13c9b8b88177d7172080cafd084e5f6432e972d
Elodie <72509016+Elo-mars@users.noreply.github.com>
2025-08-19 15:58:37 +0200

    Update pre_processing_each_sample.R

--- pre_processing_each_sample.R
+++ pre_processing_each_sample.R
@@ -2,7 +2,7 @@
 SAMPLE="GC110700_SI-GA-D1" # sample name of the directory where the data is
 ORGANISM="Human" # or "Mouse"
 name="GC110700_SI-GA-D1" # nice name you want to see in your plots
-
+reslist<-c(0.4,0.5,0.6,0.7,0.8,0.9,1,1.1,1.2,1.3,1.4)
 # load packages:
 library("Seurat")                                                   
 library("plyr")                                                                                                                         
@@ -85,16 +85,12 @@ sample
 # ================= #
 # cell cycle effect #
 # ================= #
-# remove gene cycle genes: 
+
 sample<- NormalizeData(sample, verbose = FALSE)
 gene.use=setdiff(rownames(sample),c(cc.genes.updated.2019$s.genes,cc.genes.updated.2019$g2m.genes))
 
 FHF <- CellCycleScoring(sample, s.features = cc.genes.updated.2019$s.genes, g2m.features = cc.genes.updated.2019$g2m.genes, set.ident = TRUE)
-#  install.packages("SeuratObject")                                                                                                                                                                         
-
 count.data <- GetAssayData(object = FHF[["RNA"]])[gene.use,]                                                                                                                                                                      
-
-# count.data <- as.matrix(x = count.data + 1)                                                                                                                                                                                       
 
 FHF2 <- SetAssayData(
   object = subset(FHF,features=gene.use),  
@@ -104,27 +100,21 @@ FHF2 <- SetAssayData(
 
 sample<-FHF2
 
-
-
 # ==================== #
 # Normalizing the data #
 # ==================== #
 
-# After removing unwanted cells from the dataset, the next step is to normalize the data.
-# # PCA
 Norm_sample <- NormalizeData(sample, normalization.method = "LogNormalize", scale.factor = 10000)
 
 # ============================================================== #
 # Identification of highly variable features (feature selection) #
 # ============================================================== #
 
-#PCA
 FHF <- FindVariableFeatures(Norm_sample, selection.method = "vst", nfeatures = 2000)
 FHF
 # Identify the 10 most highly variable genes
 top10 <- head(VariableFeatures(FHF), 10)
 
-# plot variable features with and without labels
 plot1 <- VariableFeaturePlot(FHF)
 plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
 pdf(paste0(opath,name,"_Highly_Variable_genes.pdf"), width=10, height=5)
@@ -137,32 +127,17 @@ length(all.genes)
 # ===== #
 # Scale #
 # ===== #
-saveRDS(FHF, file =paste0(opath,name,"_B4_regression.rds"))
-# Scaled <- ScaleData(FHF, features = all.genes)
-#saveRDS(Scaled, file =paste0(opath,name,"_Scaled.rds"))
-Scaled <- ScaleData(FHF, features = gene.use, vars.to.regress = c("nCount_RNA", "percent.mt","S.Score", "G2M.Score")) # regress on cell cycle scores after remove them 
 
-saveRDS(Scaled, file =paste0(opath,name,"_AFT_regression.rds"))
-
-
-
-
-# ===== #
-# Scale #
-# ===== #
-
-# Scaled <- ScaleData(FHF, features = all.genes)
-#saveRDS(Scaled, file =paste0(opath,name,"_Scaled.rds"))
-#Scaled <- ScaleData(FHF, features = all.genes, vars.to.regress = c("nCount_RNA", "percent.mt","S.Score", "G2M.Score")) # regress on cell cycle scores? or remove ? 
+Scaled <- ScaleData(FHF, features = gene.use, vars.to.regress = c("nCount_RNA", "percent.mt","S.Score", "G2M.Score"))
 
 # ======= #
 # Run PCA #
 # ======= #
 
 # library(future)
-# plan("multiprocess", workers = 60) # 60 cores/CPU
-# options(future.globals.maxSize = 10000 * 1024^2) # 10G RAM
-#
+# plan("multiprocess", workers = 60)
+# options(future.globals.maxSize = 10000 * 1024^2)
+
 Scaled <- RunPCA(Scaled, features = VariableFeatures(Scaled), npcs = 30, ndims.print = 1:5, nfeatures.print = 30)
 
 pdf(paste0(opath,name,"_VizDim.pdf"))
@@ -181,8 +156,6 @@ dev.off()
 # Determine statistically significant principal components #
 # ======================================================== #
 
-# plan("multiprocess", workers = 60) # uses 45 CPU
-# options(future.globals.maxSize = 10000 * 1024^2) # set it at 10Go of RAM)
 Scaled <- JackStraw(Scaled, num.replicate = 100, dims = 30)
 Scaled <- ScoreJackStraw(Scaled, dims = 1:30)
 
@@ -198,11 +171,9 @@ NPC=30
 # ==================== #
 # 3) Cluster the cells #
 # ==================== #
+
 Scaled <- RunUMAP(Scaled, reduction = "pca", dims = 1:30)
 FN <- FindNeighbors(Scaled, reduction = "pca", dims = 1:NPC, k.param = 30, compute.SNN = TRUE, nn.method = "rann", nn.eps = 0)
-# plan("multiprocess", workers = 60) # uses 45 CPU
-# options(future.globals.maxSize = 10000 * 1024^2) # set it at 10Go of RAM)
-
 FC <- FindClusters(FN, resolution = reslist)
 
 saveRDS(FC, paste0(opath,name,"_FC.rds"))
@@ -219,7 +190,6 @@ Seurat_per_sample_Ident <- SetIdent(object = FC, value = 'orig.ident')
 Seurat_per_sample <- RunUMAP(Seurat_per_sample_Ident, dims = 1:NPC)
 Seurat_per_sample
 
-# remove "plot.title" now it is a "+ labs"
 pdf(paste0(opath,name,"_Plot_per_sample.pdf"))
 UMAPPlot(Seurat_per_sample, reduction = "umap", pt.size=0.1,label=FALSE) +labs(title = "UMAP per sample")+ theme(plot.title = element_text(hjust = 0.5))
 dev.off()
@@ -229,7 +199,6 @@ UMAPPlot(Seurat_per_sample, reduction = "umap", pt.size=0.1,label=FALSE) +labs(t
 dev.off()
 
 head(FC@meta.data)
-
 
 pdf(paste0(opath,name,"_Clusters_diff_res.pdf"))
 plotlist <- list()
@@ -248,7 +217,6 @@ head(FC@meta.data)
 u=FC@meta.data
 write.table(u, paste0(opath,name,"_metadata.txt"),col.names=NA, sep="\t")
 
-
 # tables - number of cells per cluster for each resolution
 for (i in (1:length(reslist))){
   res_i<-paste0('RNA_snn_res.',reslist[i])
@@ -259,52 +227,6 @@ for (i in (1:length(reslist))){
   write.table(cell_perC, paste0(opath,name,"Integrated_","_res0",reslist[i],"_cell_per_c.txt"),col.names=NA, sep="\t")
 }
 
-
-# FAM for each resolution
-# with heatmaps with different #genes/cluster
-FC<-readRDS(file=paste0(opath,name,'_FC.rds'))
-library(future)
-for (i in (1:length(reslist))){
-  res_i<-paste0('RNA_snn_res.',reslist[i])
-  
-  Seurat_reso <- SetIdent(object = FC, value = res_i)
-  Seurat.markers<-FindAllMarkers(Seurat_reso, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-  # Seurat.markers <- FindAllMarkers(Seurat_reso,test.use="MAST",  min.pct = 0, logfc.threshold = 0, max.cells.per.ident = Inf, latent.vars = NULL,min.cells.feature = 0, min.cells.group = 0)
-  write.table(Seurat.markers, paste0(opath,name,"_res0",reslist[i],"_MAST_markers_unannotated_Clusters.txt"),col.names=NA, sep="\t")
-  tsne.markers=Seurat.markers
-  #DefaultAssay(Seurat_reso) <- 'integrated'
-  topn = tsne.markers %>% group_by(cluster) %>% top_n(10, avg_log2FC)
-  topn$gene
-  pdf(paste0(opath,name,"_res0",reslist[i]*10,"_heatmap_top10genes.pdf"),width=25, height=20)
-  q<-DoHeatmap(object = Seurat_reso, features=topn$gene, group.by = "ident", label = TRUE, angle=45) + ggtitle("top 10 genes expressed per cluster") + NoLegend()
-  print(q)
-  dev.off()
-  topn = tsne.markers %>% group_by(cluster) %>% top_n(15, avg_log2FC)
-  topn$gene
-  pdf(paste0(opath,name,"_res0",reslist[i]*10,"_heatmap_top15genes.pdf"),width=25, height=25)
-  q<-DoHeatmap(object = Seurat_reso, features=topn$gene, group.by = "ident", label = TRUE, angle=45) + ggtitle("top 15 genes expressed per cluster") + NoLegend()
-  print(q)
-  dev.off()
-  topn = tsne.markers %>% group_by(cluster) %>% top_n(20, avg_log2FC)
-  topn$gene
-  pdf(paste0(opath,name,"_res0",reslist[i]*10,"_heatmap_top20genes.pdf"),width=25, height=30)
-  q<-DoHeatmap(object = Seurat_reso, features=topn$gene, group.by = "ident", label = TRUE, angle=45) + ggtitle("top 20 genes expressed per cluster") + NoLegend()
-  print(q)
-  dev.off()
-  topn = tsne.markers %>% group_by(cluster) %>% top_n(5, avg_log2FC)
-  topn$gene
-  pdf(paste0(opath,name,"_res0",reslist[i]*10,"_heatmap_top5genes.pdf"),width=25, height=15)
-  q<-DoHeatmap(object = Seurat_reso, features=topn$gene, group.by = "ident", label = TRUE, angle=45) + ggtitle("top 5 genes expressed per cluster") + NoLegend()
-  print(q)
-  dev.off()
-  topn = tsne.markers %>% group_by(cluster) %>% top_n(5, avg_log2FC)
-  topn$gene
-  topn
-  write_csv(topn, paste0(opath,name,"_res0",reslist[i]*10,"_top5genespercluster_logFC.csv"))
-  pdf(paste0(opath,name,"_res0",reslist[i]*10,"_VlnPlot.pdf"), width = 15, height = 8)
-  VlnPlot(Seurat_reso, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), pt.size = 0.5,ncol = 3)
-  dev.off()  
-}
 q()
 n
 
